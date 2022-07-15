1d8d77aa742e8bbc56307f437496bff8fae6318a
A-A-Tyurin <avt2@yandex.ru>
2022-07-15 06:32:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -132,6 +132,6 @@ if __name__ == '__main__':
     updater.start_webhook(
         listen='0.0.0.0',
         port=HEROKU_PORT,
-        url_path=TELEGRAM_TOKEN
+        url_path=TELEGRAM_TOKEN,
+        webhook_url=f'https://{HEROKU_APP_NAME}.herokuapp.com/{TELEGRAM_TOKEN}'
     )
-    updater.bot.set_webhook(f'https://{HEROKU_APP_NAME}.herokuapp.com/{TELEGRAM_TOKEN}')
